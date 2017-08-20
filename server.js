09632676386db6b3777c2b28c5986c14aafca6fd
33 <suguojun126@163.com>
2017-08-20 21:07:13 +0800

    demo

---
+++ server.js
@@ -0,0 +1,60 @@
+// var http = require("http");
+// var url = require("url");
+
+// function start(route) {
+//   function onRequest(request, response) {
+//     var pathname = url.parse(request.url).pathname;
+//     console.log("Request for " + pathname + " received.");
+
+//     console.log(url.parse(request.url).query)
+
+//     route(pathname);
+
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.write("Hello World");
+//     response.end();
+//   }
+
+//   http.createServer(onRequest).listen(8888);
+//   console.log("Server has started.");
+// }
+
+// exports.start = start;
+
+// var http = require("http");
+// var url = require("url");
+
+// function start(route, handle) {
+//   function onRequest(request, response) {
+//     var pathname = url.parse(request.url).pathname;
+//     console.log("Request for " + pathname + " received.");
+
+//     route(handle, pathname);
+
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.write("Hello World");
+//     response.end();
+//   }
+
+//   http.createServer(onRequest).listen(8888);
+//   console.log("Server has started.");
+// }
+
+// exports.start = start;
+
+var http = require("http");
+var url = require("url");
+
+function start(route, handle) {
+  function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
+
+    route(handle, pathname, response);
+  }
+
+  http.createServer(onRequest).listen(8888);
+  console.log("Server has started.");
+}
+
+exports.start = start;
